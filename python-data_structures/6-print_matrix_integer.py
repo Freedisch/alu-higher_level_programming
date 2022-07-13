470f57d24d801eacd7a74254110df61fc572a19c
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-07-14 00:01:01 +0200

    Hope

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,6 @@
 def print_matrix_integer(maxtrix=[[]]):
     if maxtrix:
         for row in maxtrix:
-            line_to_print = ""
             for num in row:
-                print("{:d}".format(num), end=" " if num !=[-1] else "")
+                print("{:d}".format(num), end=" " if num != row[-1] else "")
             print()
