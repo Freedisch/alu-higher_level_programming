5a4b1ddaeabcf3bb8c3263b32b4496fecc16bc88
Freedisch <freeproduc@gamil.com>
2022-07-15 20:04:29 +0200

    rebase

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list == 0):
-        return None
+    if len(my_list) == 0:
+        return "None"
     else:
-        max = list[0]
+        max = my_list[0]
         for i in range(len(my_list)):
-            max = my_list [i]
             if my_list[i] > max:
                 max = my_list[i]
-            return my_list
+        return max
