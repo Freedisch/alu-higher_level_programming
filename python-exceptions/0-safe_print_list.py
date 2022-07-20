6057ffd1aa0b34bfe8740147e008047ae1283f08
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-07-20 11:26:37 +0200

    review 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(my_list):
         try:
             print("{}".format(my_list[i]), end="")
-            Nmbrep+=1
+            Nmbrep += 1
         except IndexError:
             break
-        print("")
+    print("")
     return Nmbrep
