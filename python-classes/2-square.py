a6d354f9f3585ac97d7dd997d93aa1c16e1326ab
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-07-21 22:13:38 +0200

    task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Raising errors for the size"""
     def __init__(self, size=0):
-        if size:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
                 raise ValueError("size must be >= 0")
