a2b95710e4e32090c85e16555aaafa1a2c76b62a
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-07-22 19:55:52 +0200

    task

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
     def height(self):
         """Get the height of the rectangle"""
         return self.__height
-        
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -33,9 +33,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+        
     def area(self):
         """Return the area of the rectangle"""
         return (self.__width * self.__height)
+
     def perimeter(self):
         """Calculate the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
