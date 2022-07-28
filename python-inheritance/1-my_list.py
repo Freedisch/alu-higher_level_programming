9431d0cb246296cdeccc1b87db4cf5a0ce30ea86
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-07-28 11:03:38 +0200

    tegte

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,9 +4,6 @@
 
 class Mylist(list):
     """"Shit I forgot"""
-    def __init__(self):
-        """opoofod"""
-        pass
     def print_sorted(self):
         """Pfff"""
-        return self.sort
+        print(sorted(self))
