88abe47fb5dc53f522eb5ea9524055ba1bae1931
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-07-28 11:06:00 +0200

    retete

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""Inheritance"""
+"""
+    1-my_list: class MyList
+"""
 
 
-class Mylist(list):
-    """"Shit I forgot"""
+class MyList(list):
+    """
+        This class inherits from list.
+        Attributes:
+        Methods:
+            print_sorted - prints the list in ascending order
+    """
     def print_sorted(self):
-        """ 
-            fuck oooooooooooofo
+        """
+           prints a list in ascending order.
         """
         print(sorted(self))
