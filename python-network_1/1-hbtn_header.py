6ef60d29d057f29d9e0529ca18ec3387ef7b7523
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-05 11:06:13 +0200

    docccc

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
-""" Response header """
+"""Takes in a URL, sends a request to the URL and displays the value of the
+X-Request-Id variable found in the header of the response"""
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    """Documented"""
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-    
-    head = response.headers.get('X-Request-Id')
-    print(head)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        head = response.headers.get('X-Request-Id')
+        print(head)
