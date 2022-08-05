eb747e1486d4d8e071883f9ecd23239b9ef23875
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-05 11:33:09 +0200

    Correction error_py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -7,7 +7,10 @@ from sys import argv
 
 if __name__ == "__main__":
     res = requests.get(argv[1])
-    if res.status_code >= 400:
-        print("Error code: {}".format(res.status_code))
-    else:
-        print(res.text)
+     try:
+        if res.status_code > 400:
+            print("Error code: {}".format(res.status_code))
+        else:
+            print(res.content.decode("utf-8"))
+    except KeyError:
+        pass
