85477d6cd9ed6ab18493d3a3d8f656df63f48764
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-05 10:58:31 +0200

    Task 1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+""" Response header """
+
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    head = response.headers.get('X-Request-Id')
+    print(head)
