3c24abd0b08abe8be126d7ccd59e186abf8df410
Freedisch <freeproduc@gmail.com>
2022-08-05 17:41:38 +0200

    module

--- python-network_1/9-starwars.py
+++ python-network_1/9-starwars.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    r = requests.get('https://swapi.co/api/people/?search={}'
-                     .format(sys.argv[1])).json()
+    r = requests.get('https://swapi.co/api/people/?search={}'.format(sys.argv[1])).json()
     print('Number of results: {}'.format(r.get('count')))
     for res in r.get('results'):
         print(res.get('name'))
