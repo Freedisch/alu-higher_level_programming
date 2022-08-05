112e4643c47cf6d9843ed31fb972548f5cf18b58
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-05 11:36:32 +0200

    poooo

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -6,6 +6,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
+    """Takes in a URL, sends a request to the URL and
+       displays the body of the response"""
     res = requests.get(argv[1])
      try:
          if res.status_code > 400:
