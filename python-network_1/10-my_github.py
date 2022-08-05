e56213b4c9283089957cab7c3a589c2f5ec2139d
Freedisch <freeproduc@gmail.com>
2022-08-05 17:45:56 +0200

    module error

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-import requests
-import sys
 """Takes in Github credentials (username and password) and uses the Github API
 to display an id"""
 
 
-r = requests.get('https://api.github.com/user', auth=(sys.argv[2]))
-if r.status_code >= 400:
-    print('None')
-else:
-    print(r.json().get('id'))
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        print(r.json().get('id'))
