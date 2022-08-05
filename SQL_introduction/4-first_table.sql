c37e7c1542d0f022c8603e5dba27ab0452861c79
Freedisch <freeproduc@gmail.com>
2022-08-05 19:15:26 +0200

    review

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
 -- creates a table called first_table in the current database in your MySQL server
--- creates a table even if the table already exists
 CREATE TABLE IF NOT EXITS first_table (id INT, name VARCHAR(256));
