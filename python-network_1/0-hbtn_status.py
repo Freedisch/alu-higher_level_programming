212e33183ebce0dcec56d33fc9bf256a9dc48be5
Freedisch <freeproduc@gmail.com>
2022-08-05 17:34:37 +0200

    Ddos unix error

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
-""" Fetching Url """
+"""Fetches https://intranet.hbtn.io/status"""
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
