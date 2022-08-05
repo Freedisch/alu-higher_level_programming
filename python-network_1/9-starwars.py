7fc85abeadfd33ece9eb7fea6d362bdb43e06611
Freedisch <freeproduc@gmail.com>
2022-08-05 17:35:15 +0200

    formatting error

--- python-network_1/9-starwars.py
+++ python-network_1/9-starwars.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Takes in Github credentials (username and password) and uses the Github API
-to display an id"""
-
+"""Takes in a string and sends a search request to the Star Wars API"""
 
 if __name__ == "__main__":
     import requests
     import sys
 
-    r = requests.get('https://api.github.com/user',
-                     auth=(sys.argv[1], sys.argv[2]))
-    if r.status_code >= 400:
-        print('None')
-    else:
-        print(r.json().get('id'))
+    r = requests.get('https://swapi.co/api/people/?search={}'
+                     .format(sys.argv[1])).json()
+    print('Number of results: {}'.format(r.get('count')))
+    for res in r.get('results'):
+        print(res.get('name'))
