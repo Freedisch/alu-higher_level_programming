4d9ee7b04b94f6b1006b15eb50c1ef8f7af94f26
Freedisch <freeproduc@gmail.com>
2022-08-22 20:37:30 +0200

    humm

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,7 +8,7 @@ Returns an integer: the addition of a and b
 
 
 
-def add_integer(a, b=98):
+def add_integer(a, b):
     """Find somme of 2 integer"""
     if type(a) is not int and type (a) is not float:
         raise TypeError("a must be an integer")
