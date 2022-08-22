739a0cac6b42b52613ed8a00e101b2b4f76a8a65
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-22 21:47:41 +0200

    updt

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
 Prototype: def add_integer(a, b=98):
-a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
 """
 
 
 def add_integer(a, b):
     """Find somme of 2 integer"""
     if type(a) is not int and type (a) is not float:
-        raise TypeError("a must be an integer")
+        raise TypeError ("a must be an integer")
     if type(b) is not int and type (b) is not float:
-        raise TypeError("b must be an integer")
+        raise TypeError ("b must be an integer")
     return (int(a) + int(b))
