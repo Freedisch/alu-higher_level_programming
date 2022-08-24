a8d8f72712fb8fa329a994cadefc456e9b690316
Freedisch <freeproduc@gmail.com>
2022-08-24 14:49:24 +0200

    0psycide

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     import sys
 
     db = MySQLdb.connect(host='localhost', port=3306,
-                        user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
 
     launcher = db.cursor()
     launcher.execute("SELECT * FROM states ORDER BY states.id;")
