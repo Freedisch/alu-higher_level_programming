840207a1bbf357f0be04c79bfb5a5d5d4130a5d3
Freedisch <freeproduc@gmail.com>
2022-08-24 14:07:44 +0200

    Selecting states id

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyhton3
+"""Selecting all the states form hbtn_0e_0usa"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306, user= sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+
+    launcher = db.cursor()
+    launcher.execute("SELECT * FROM states BY sates.id;")
+    rows = launcher.fetchall()
+    for row in rows:
+        print(row)
