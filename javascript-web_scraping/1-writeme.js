8565fe950f09bed536d34811cff287698dadd777
Freedisch <freeproduc@gmail.com>
2022-10-14 14:13:37 +0200

    Semi standard error

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,9 +2,10 @@
 
 const fs = require('fs');
 
-try{
-    fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (error, result) => {
-        if (error) return console.log(error)});
-}catch(error){
-    console.log(error);
+try {
+  fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (error, result) => {
+    if (error) return console.log(error);
+  });
+} catch (error) {
+  console.log(error);
 }
