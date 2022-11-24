5de7bf6200b86030eab4860844f17690f3cb0924
Freedisch <freeproduc@gmail.com>
2022-11-25 00:13:11 +0200

    ✅final review

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) console.log('Not a number');
-else console.log(parseInt(process.argv[2]));
+else console.log("My number: " + parseInt(process.argv[2]));
