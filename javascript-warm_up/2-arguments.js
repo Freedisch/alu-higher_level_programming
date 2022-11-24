9f11baa6a3c055dfb8e5746bb414460a4624d39e
Freedisch <freeproduc@gmail.com>
2022-11-25 00:27:39 +0200

    arguments error fixed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const array = process.argv.slice(2);
 if (array.length === 0) console.log('No argument');
-else console.log('Argument found');
+else if (array.length > 1) console.log('Arguments found');
+else if (array.length === 1) console.log('Argument found');
