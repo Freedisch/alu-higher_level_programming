c6351395d6ed2e6711669180b6c7c85887f0fa68
Freedisch <freeproduc@gmail.com>
2022-11-25 00:52:56 +0200

    fixed sorting error

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 
-const array = process.argv.slice(2);
+const list = process.argv.slice(2);
+const array = list.map(element => parseInt(element));
 if (array.length <= 1) {
   console.log(0);
 } else {
-  array.sort();
+  array.sort((a, b) => {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  });
   console.log(array[array.length - 2]);
 }
